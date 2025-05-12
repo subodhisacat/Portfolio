a2e36a3b61b2fa8edc32309920637ea5bc552158
Subodh ‎ <subodhfree66@gmail.com>
2025-05-12 22:22:16 +0545

    initial cmit

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
-
-export default defineConfig({
-  base: "./",
-  plugins: [viteReact(), VitePWA()],
-});
