f52a9b60e4c423e765fa8840e7ef7f8412841d8b
Subodh Timalsina <108208948+subodhisacat@users.noreply.github.com>
2025-12-23 19:59:33 +0545

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./index.css";
 import App from "./App";
 
